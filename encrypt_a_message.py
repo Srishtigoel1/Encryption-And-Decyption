8caeb760f09b20233fd360f410f605b8b008164c
Srishtigoel1 <112386808+Srishtigoel1@users.noreply.github.com>
2024-05-29 18:38:47 +0530

    Update encrypt_a_message.py

--- encrypt_a_message.py
+++ encrypt_a_message.py
@@ -18,12 +18,4 @@ print(f"original message: {plain_text}")
 print(f"encrypted message: {cipher_text}")
 
 print("\n")
-#decyption
-cipher_text=input("Enter a message to decrypt")
-plain_text=""
-for letter in cipher_text:
-    index=key.index(letter)
-    plain_text+=chars[index]
-    
-print(f"encrypted message: {cipher_text}")
-print(f"original message was: {plain_text}")
+
